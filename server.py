869523e01003ba97619ccab22b132ae23d22a26b
Brian Denmark <testingarea479@gmail.com>
2022-08-27 00:11:38 -0700

    further updated styles/content/etc.

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ def guess():
         return redirect('/')
 
     else: 
-        session['message'] = "YOU DID IT!"
+        session['message'] = "Yeah, yeah, but your scientists were so preoccupied with whether or not they could that they didn't stop to think if they should"
         return render_template('victory.html')
 
 @app.route('/destroy', methods=['GET'])
